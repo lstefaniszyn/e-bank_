c01f6d18d4027dc21f3ba2a7453eb8fed5b2348d
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2020-10-16 19:26:57 +0200

    added Swagger annotions (#7)

--- src/main/java/com/example/ebank/controllers/CustomerController.java
+++ src/main/java/com/example/ebank/controllers/CustomerController.java
@@ -2,30 +2,51 @@ package com.example.ebank.controllers;
 
 import com.example.ebank.models.Customer;
 import com.example.ebank.services.CustomerService;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value = "clients", description = "the clients API")
+
 @RestController
-@RequestMapping("/api/v1/customers") //todo
+@RequestMapping("/v1/customers")
 public class CustomerController {
 
-    private final CustomerService customerService;
+        private final CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+        public CustomerController(CustomerService customerService) {
+                this.customerService = customerService;
+        }
 
-    @GetMapping
-    public Iterable<Customer> list() {
-        return customerService.getAll();
-    }
+        @ApiOperation(value = "Get all clients", nickname = "list", notes = "", response = Customer.class, responseContainer = "List", tags = {
+                        "client", })
+        @ApiResponses(value = {
+                        @ApiResponse(code = 200, message = "successful operation", response = Customer.class, responseContainer = "List"),
+                        @ApiResponse(code = 400, message = "Invalid client id supplied"),
+                        @ApiResponse(code = 404, message = "Client not found") })
+        @RequestMapping(value = "/", produces = { "application/json" }, method = RequestMethod.GET)
+        public Iterable<Customer> list() {
+                return customerService.getAll();
+        }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Customer> get(@PathVariable Long id) {
-        return ResponseEntity.ok(customerService.getOne(id));
-    }
+        @ApiOperation(value = "Get client by id", nickname = "get", notes = "", response = Customer.class, tags = {
+                        "client", })
+        @ApiResponses(value = { @ApiResponse(code = 200, message = "successful operation", response = Customer.class),
+                        @ApiResponse(code = 400, message = "Invalid client id supplied"),
+                        @ApiResponse(code = 404, message = "Client not found") })
+        @RequestMapping(value = "/{id}", produces = { "application/json" }, method = RequestMethod.GET)
+        public ResponseEntity<Customer> get(
+                        @ApiParam(value = "The id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long id) {
+                return ResponseEntity.ok(customerService.getOne(id));
+        }
 
 }
--- src/main/java/com/example/ebank/controllers/TransactionController.java
+++ src/main/java/com/example/ebank/controllers/TransactionController.java
@@ -9,30 +9,42 @@ import java.util.List;
 import com.example.ebank.models.Transaction;
 import com.example.ebank.services.TransactionService;
 
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
-@RequestMapping("/api/v1/transactions")
+@RequestMapping("/v1/transactions")
 public class TransactionController {
 
     private final static String DATE_FORMAT = "yyyy-MM";
     private final static DateTimeFormatter DATE_FORMATTER = new DateTimeFormatterBuilder()
-            .append(DateTimeFormatter.ofPattern(DATE_FORMAT)).parseDefaulting(ChronoField.DAY_OF_MONTH, 1).toFormatter();
+            .append(DateTimeFormatter.ofPattern(DATE_FORMAT)).parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
+            .toFormatter();
 
     @Autowired
     private TransactionService transactionService;
 
-    @GetMapping
+    @ApiOperation(value = "Get account's transactions", nickname = "getAllByDatePaginated", notes = "", response = Transaction.class, responseContainer = "List", tags = {
+            "account", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "successful operation", response = Transaction.class, responseContainer = "List"),
+            @ApiResponse(code = 400, message = "Invalid client id supplied"),
+            @ApiResponse(code = 404, message = "Client not found") })
+    @RequestMapping(value = "/", produces = { "application/json" }, method = RequestMethod.GET)
     public List<Transaction> getAllByDatePaginated(
-            @RequestParam(name = "date") @DateTimeFormat(pattern = DATE_FORMAT) String dateString,
-            @RequestParam(name = "page", defaultValue = "0") int page,
-            @RequestParam(name = "size", defaultValue = "2") int size) {
+            @ApiParam(value = "The date to filter. Use \"2020-10\" for testing. ", required = true) @RequestParam(name = "date") @DateTimeFormat(pattern = DATE_FORMAT) String dateString,
+            @ApiParam(value = "The Page number to fetched. Use \"0\" for testing. ", required = false) @RequestParam(name = "page", defaultValue = "0") int page,
+            @ApiParam(value = "The number of objects fetch. Use \"2\" for testing. ", required = false) @RequestParam(name = "size", defaultValue = "2") int size) {
         LocalDate date = LocalDate.parse(dateString, DATE_FORMATTER);
         Page<Transaction> resultsPage = transactionService.findInMonthPaginated(date, page, size);
         return resultsPage.getContent();
