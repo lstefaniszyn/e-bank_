9afe436e0fdf8dc24bc91db394b47426f21edb10
gwlodawiec <gwlodawiec@gmail.com>
2020-10-19 11:15:10 +0200

    swagger api definition fix

--- src/main/java/com/example/ebank/controllers/CustomerController.java
+++ src/main/java/com/example/ebank/controllers/CustomerController.java
@@ -33,64 +33,70 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
-	
-		private final static String DATE_FORMAT = "yyyy-MM";
-		private final static DateTimeFormatter DATE_FORMATTER = new DateTimeFormatterBuilder()
-            .append(DateTimeFormatter.ofPattern(DATE_FORMAT)).parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
-            .toFormatter();
 
-        private final CustomerService customerService;
-        private final TransactionService transactionService;
-        private final AccountService accountService;
+	private final static String DATE_FORMAT = "yyyy-MM";
+	private final static DateTimeFormatter DATE_FORMATTER = new DateTimeFormatterBuilder()
+			.append(DateTimeFormatter.ofPattern(DATE_FORMAT)).parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
+			.toFormatter();
 
-        public CustomerController(CustomerService customerService, TransactionService transactionService,
-    			AccountService accountService) {
-        	
-                this.customerService = customerService;
-                this.transactionService = transactionService;
-        		this.accountService = accountService;
-        }
+	private final CustomerService customerService;
+	private final TransactionService transactionService;
+	private final AccountService accountService;
 
-        @ApiOperation(value = "Get all clients", nickname = "list", notes = "", response = Customer.class, responseContainer = "List", tags = {
-                        "client", })
-        @ApiResponses(value = {
-                        @ApiResponse(code = 200, message = "successful operation", response = Customer.class, responseContainer = "List"),
-                        @ApiResponse(code = 400, message = "Invalid client id supplied"),
-                        @ApiResponse(code = 404, message = "Client not found") })
-        @RequestMapping(value = "/", produces = { "application/json" }, method = RequestMethod.GET)
-        public Iterable<Customer> list() {
-                return customerService.getAll();
-        }
+	public CustomerController(CustomerService customerService, TransactionService transactionService,
+			AccountService accountService) {
 
-        @ApiOperation(value = "Get client by id", nickname = "get", notes = "", response = Customer.class, tags = {
-                        "client", })
-        @ApiResponses(value = { @ApiResponse(code = 200, message = "successful operation", response = Customer.class),
-                        @ApiResponse(code = 400, message = "Invalid client id supplied"),
-                        @ApiResponse(code = 404, message = "Client not found") })
-        @RequestMapping(value = "/{id}", produces = { "application/json" }, method = RequestMethod.GET)
-        public ResponseEntity<Customer> get(
-                        @ApiParam(value = "The id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long id) {
-                return ResponseEntity.ok(customerService.getOne(id));
-        }
-        
-        @RequestMapping(value = "/{id}/accounts/{accountId}/transactions", produces = {"application/json" }, method = RequestMethod.GET)
-		public ResponseEntity<List<Transaction>> getTransactions(
-				@ApiParam(value = "The customer id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long id,
-				@ApiParam(value = "The account id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long accountId,
-				@ApiParam(value = "The date to filter. Use \"2020-10\" for testing. ", required = true) @RequestParam(name = "date") @DateTimeFormat(pattern = DATE_FORMAT) String dateString,
-				@ApiParam(value = "The Page number to fetched. Use \"0\" for testing. ", required = false) @RequestParam(name = "page", defaultValue = "0") int page,
-				@ApiParam(value = "The number of objects fetch. Use \"2\" for testing. ", required = false) @RequestParam(name = "size", defaultValue = "2") int size) {
-			
-			Customer customer = customerService.getOne(id);
-			Account account = accountService.getOne(accountId);
-			if(!account.getCustomer().getId().equals(customer.getId())) {
-				throw new IllegalArgumentException();
-			}
-			
-			LocalDate date = LocalDate.parse(dateString, DATE_FORMATTER);
-			Page<Transaction> resultPage = transactionService.findForAccountInMonthPaginated(accountId, date, page, size);
-			
-			return ResponseEntity.ok(resultPage.getContent());
+		this.customerService = customerService;
+		this.transactionService = transactionService;
+		this.accountService = accountService;
+	}
+
+	@ApiOperation(value = "Get all clients", nickname = "list", notes = "", response = Customer.class, responseContainer = "List", tags = {
+			"client", })
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "successful operation", response = Customer.class, responseContainer = "List"),
+			@ApiResponse(code = 400, message = "Invalid client id supplied"),
+			@ApiResponse(code = 404, message = "Client not found") })
+	@RequestMapping(value = "/", produces = { "application/json" }, method = RequestMethod.GET)
+	public Iterable<Customer> list() {
+		return customerService.getAll();
+	}
+
+	@ApiOperation(value = "Get client by id", nickname = "get", notes = "", response = Customer.class, tags = {
+			"client", })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "successful operation", response = Customer.class),
+			@ApiResponse(code = 400, message = "Invalid client id supplied"),
+			@ApiResponse(code = 404, message = "Client not found") })
+	@RequestMapping(value = "/{id}", produces = { "application/json" }, method = RequestMethod.GET)
+	public ResponseEntity<Customer> get(
+			@ApiParam(value = "The id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long id) {
+		return ResponseEntity.ok(customerService.getOne(id));
+	}
+
+	@ApiOperation(value = "Get transactions for given user and account and date", nickname = "get", notes = "", response = Transaction.class, tags = {
+			"client", })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "successful operation", response = Customer.class),
+			@ApiResponse(code = 400, message = "Invalid client id supplied"),
+			@ApiResponse(code = 404, message = "Client not found") })
+	@RequestMapping(value = "/{id}/accounts/{accountId}/transactions", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public ResponseEntity<List<Transaction>> getTransactions(
+			@ApiParam(value = "The customer id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long id,
+			@ApiParam(value = "The account id that needs to be fetched. Use \"1\" for testing. ", required = true) @PathVariable Long accountId,
+			@ApiParam(value = "The date to filter. Use \"2020-10\" for testing. ", required = true) @RequestParam(name = "date") @DateTimeFormat(pattern = DATE_FORMAT) String dateString,
+			@ApiParam(value = "The Page number to fetched. Use \"0\" for testing. ", required = false) @RequestParam(name = "page", defaultValue = "0") int page,
+			@ApiParam(value = "The number of objects fetch. Use \"2\" for testing. ", required = false) @RequestParam(name = "size", defaultValue = "2") int size) {
+
+		Customer customer = customerService.getOne(id);
+		Account account = accountService.getOne(accountId);
+		if (!account.getCustomer().getId().equals(customer.getId())) {
+			throw new IllegalArgumentException();
 		}
+
+		LocalDate date = LocalDate.parse(dateString, DATE_FORMATTER);
+		Page<Transaction> resultPage = transactionService.findForAccountInMonthPaginated(accountId, date, page, size);
+
+		return ResponseEntity.ok(resultPage.getContent());
+	}
 
 }
