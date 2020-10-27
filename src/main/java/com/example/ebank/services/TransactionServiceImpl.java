7de5046b4f8291438039d9d7a7aed5e908714bd5
tompalka <tomasz.palka@capgemini.com>
2020-10-27 15:31:44 +0100

    Add code for converting transactions currency

--- src/main/java/com/example/ebank/services/TransactionServiceImpl.java
+++ src/main/java/com/example/ebank/services/TransactionServiceImpl.java
@@ -1,20 +1,25 @@
 package com.example.ebank.services;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.ebank.models.Currency;
 import com.example.ebank.models.Transaction;
 import com.example.ebank.repositories.TransactionRepository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.Optional;
-
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final static Currency TARGET_CURRENCY = Currency.GBP;
 
     public TransactionServiceImpl(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
@@ -29,13 +34,32 @@ public class TransactionServiceImpl implements TransactionService {
         return transactionRepository.findById(id);
     }
 
+    private static double getExchangeRate(Currency currency) {
+        return 2.0;
+    }
+
     @Override
     public Page<Transaction> findInMonthPaginated(LocalDate date, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         LocalDate startDate = date.withDayOfMonth(1);
         LocalDate endDate = date.withDayOfMonth(date.lengthOfMonth());
-        return transactionRepository.findByValueDateBetween(Date.valueOf(startDate), Date.valueOf(endDate), pageable);
-        
+        Page<Transaction> transactionsPage = transactionRepository.findByValueDateBetween(Date.valueOf(startDate),
+                Date.valueOf(endDate), pageable);
+        Map<Currency, Double> exchangeRates = new HashMap<>();
+        return transactionsPage.map(t -> {
+            if (t.getCurrency() != TARGET_CURRENCY) {
+                Transaction converted = new Transaction();
+                converted.setId(t.getId());
+                converted.setDescription(t.getDescription());
+                converted.setValueDate(t.getValueDate());
+                converted.setCurrency(TARGET_CURRENCY);
+                converted.setAmount(t.getAmount()
+                        * exchangeRates.computeIfAbsent(t.getCurrency(), TransactionServiceImpl::getExchangeRate));
+                return converted;
+            } else {
+                return t;
+            }
+        });
     }
 
     @Override
