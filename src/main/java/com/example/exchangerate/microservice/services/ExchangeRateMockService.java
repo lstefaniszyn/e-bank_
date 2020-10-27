6242e67f7e55217b51f15b2d46338bb3c8ba08a7
Palka <tomasz.palka@capgemini.com>
2020-10-27 13:45:00 +0100

    Add missing CHF currency

--- src/main/java/com/example/exchangerate/microservice/services/ExchangeRateMockService.java
+++ src/main/java/com/example/exchangerate/microservice/services/ExchangeRateMockService.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExchangeRateMockService implements ExchangeRateService {
 
-    private final Map<String, Double> exchangeRatesToEUR = Map.of("EUR", 1.0, "USD", 1.18550, "GBP", 0.90872);
+    private final Map<String, Double> exchangeRatesToEUR = Map.of("EUR", 1.0, "USD", 1.18550, "GBP", 0.90872, "CHF",
+            1.07361);
 
     @Override
     public Double getExchangeRate(String baseCurrency, String targetCurrency) {
