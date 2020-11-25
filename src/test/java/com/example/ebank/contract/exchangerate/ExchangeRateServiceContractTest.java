6a751f71265cf463871a8e6d611dfe7b9aa9263d
gwlodawiec <gwlodawiec@gmail.com>
2020-11-25 12:36:49 +0100

    code formatting

--- src/test/java/com/example/ebank/contract/exchangerate/ExchangeRateServiceContractTest.java
+++ src/test/java/com/example/ebank/contract/exchangerate/ExchangeRateServiceContractTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureStubRunner(ids = "com.example:exchangerate:+:stubs:8095", stubsMode = StubsMode.LOCAL)
 @SpringBootTest(classes = EBankApplication.class, webEnvironment = WebEnvironment.MOCK)
 @DirtiesContext
-@ActiveProfiles({"mock","it-local"})
+@ActiveProfiles({ "mock", "it-local" })
 public class ExchangeRateServiceContractTest {
 	
 	@Autowired
@@ -32,12 +32,10 @@ public class ExchangeRateServiceContractTest {
 	
 	@Test
 	public void testExchangerateEndpoint() throws Exception {
+		
 		InlineResponse200Dto response = exchangeRateService.getExchangeRate(Currency.EUR);
 		
 		assertThat(response).isNotNull();
-		
 		assertThat(response.getValue()).isEqualByComparingTo(Double.valueOf("0.90872"));
-		
 	}
-	
 }
