2cacd83e01dfb23b0896f4c3df884b1a2a63a95c
gwlodawiec <gwlodawiec@gmail.com>
2020-12-08 10:19:00 +0100

    update

--- src/test/java/com/example/ebank/arch/LayeredArchitectureTest.java
+++ src/test/java/com/example/ebank/arch/LayeredArchitectureTest.java
@@ -6,7 +6,6 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
 @AnalyzeClasses(packages = "com.example.ebank", importOptions = { ImportOption.DoNotIncludeTests.class, ImportOption.DoNotIncludeJars.class, ImportOption.DoNotIncludeArchives.class })
 public class LayeredArchitectureTest {
